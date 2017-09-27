682b6a69e8e48df77a324b547f1d3898aa692600
Justin Galea-Abela <justingabela@gmail.com>
2017-09-27 16:02:34 -0400

    Built Up Jeweller Page

--- source/scripts/public/jeweller/public.jeweller.controller.js
+++ source/scripts/public/jeweller/public.jeweller.controller.js
@@ -4,6 +4,20 @@
 
 		var vm = this;
 
+		vm.jewellers = [{
+			name: ['Justin Test'],
+			experience: 2000,
+			specialty: ['Engagement Rings']
+		},{
+			name: ['Mark Test'],
+			experience: 2010,
+			specialty: ['Necklaces']
+		},{
+			name: ['John Test'],
+			experience: 1990,
+			specialty: ['Earrings']
+		}]
+
 	}
 
 	angular
