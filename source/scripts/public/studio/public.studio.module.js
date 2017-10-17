0ceafd5c78fac22a4abc31dbcea378983f9e907c
Justin Galea-Abela <justingabela@gmail.com>
2017-10-17 14:27:53 -0400

    Built Up Studio About Bench & Equipment Pages

--- source/scripts/public/studio/public.studio.module.js
+++ source/scripts/public/studio/public.studio.module.js
@@ -3,6 +3,7 @@
 
     angular
         .module('app.public.studio', [
+	            'app.public.studio.about',
 	            'app.public.studio.bench',
 	             'app.public.studio.equipment'
         ]);
