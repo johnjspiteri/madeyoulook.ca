bb5d92c0ceb31cfda1caef607afb26a31c13926b
Mark Stanley <mark-stanley@outlook.com>
2017-11-01 09:23:13 -0400

    Created And Styled Filter Selects

--- source/scripts/public/engagement/public.engagement.controller.js
+++ source/scripts/public/engagement/public.engagement.controller.js
@@ -3,6 +3,12 @@
 	function Internal() {
 
 		var vm = this;
+		vm.search = {
+			category: ["Solitaire", "3 Stone", "Vintage", "Halo", "Modern", "Classic", "Creative", "Side Stone"],
+			type: ["Garnet", "Tourmaline", "Opal", "Aquamarine", "Citrine", "Pearl", "Promise Ring"],
+			material: ["Gold-Yellow", "Gold-White", "Gold-Rose", "Gold-Green", "Platinum", "Palladium"],
+			color: ["Black", "Blue", "Champagne", "Chocolate", "Rough"]
+		};
 
 	}
 
--- source/scripts/public/product/public.product.controller.js
+++ source/scripts/public/product/public.product.controller.js
@@ -5,6 +5,19 @@
 		var vm = this;
 
 		vm.products = productsResolve;
+		console.log('products', vm.products);
+		vm.search = {
+			category: [
+				{ title: 'Bracelet', types: ['Bangles', 'Cuff'] },
+				{ title: 'Necklace', types: ['Pendant', 'Choker', 'Lariat'] },
+				{ title: 'Ring', types: ['Sterling Silver', 'Costume'] },
+				{ title: 'Earring', types: ['Studs', 'Drops', 'Hoops'] },
+				{ title: 'Broache', types: [] },
+				{ title: 'Cufflink', types: ['Sterling Silver', 'Costume'] },
+				{ title: 'Hair Piece', types: [] }
+			],
+			material: [ "Sterling Silver", "Yellow Gold Plated", "Yellow Gold", "Rose Gold", "White Gold", "Fabric", "Rubber", "Brass", "Plexi","Leather", "Wood", "Fiber", "Plastic", "Alternative", "Glass"]
+		}
 
 	}
 
